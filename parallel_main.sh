3935144289412935a09cdce68950e74eed128df6
Philipp Herwald <pherwald@techfak.uni-bielefeld.de>
2016-06-27 17:32:21 +0200

    bash script for executing whole programm added

---
+++ parallel_main.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+
+#Script, for calling  PhySca (Main.py) parallel on different cores
+#aufzurufen
+#
+#./parallel_main.sh -nf /home/philipp/SHK_2016/Project/Input_Output/pestis_input/yersinia_species_tree -a /home/philipp/SHK_2016/Project/Input_Output/pestis_input/adjacencies -out /home/philipp/SHK_2016/Project/PhySca/pestis_testlauf -pN 3 -s 5
+#
+#make a directory for each call
+if [ $1 = "-h" ] || [ $1 = "-help" ]
+	then
+		echo "-nhx/-nf [treefile(preprocessing)] -a [adjacencies]/ -m [markers] (-out [directory for preprocessing output]) (-pN [number of processes]) (-alpha [alpha]) (-s [number of samples]) (-x [x]) "
+fi
+
+
+out="${PWD}/testlauf"
+for param in "$@"
+	do
+		parameterArray+=(${param})
+	done
+
+for((p=0;p<$#;p+=1))
+	do
+		echo ${parameterArray[$p]}
+		case ${parameterArray[$p]} in
+		-alpha) alpha=${parameterArray[$((p+1))]}
+		;;
+		-pN) processNumber=${parameterArray[$((p+1))]}
+		;;
+		-x) x=${parameterArray[$((p+1))]}
+		;;
+		-s) sampleNumber=${parameterArray[$((p+1))]}
+		;;
+		-nhx) nhx=${parameterArray[$((p+1))]}
+		;;
+		-nf) nf=${parameterArray[$((p+1))]}
+		;;
+		-a) adjacencies=${parameterArray[$((p+1))]}
+		;;
+		-m) markers=${parameterArray[$((p+1))]}}
+		;;
+		-kT) kT=${parameterArray[$((p+1))]}
+		;;
+		-out) out=${parameterArray[$((p+1))]}
+		;;
+		*)
+		;;
+		esac
+	done
+ 
+if [ -z $nhx ] && [ -z $nf ]
+	then
+		echo "Error: No tree for preprocessing given"
+		exit
+	elif [ -z $nhx  ]
+		then
+			treeparam="-nf"
+			tree="${nf}"
+
+	else
+		treeparam="-nhx"
+		tree="${nhx}"
+fi
+
+if [ -z $markers ] && [ -z $adjacencies ]
+	then
+		echo "Error:No markers or adjacencies given"
+		exit
+	elif [ -z $markers ]
+		then
+			MAparam="-a"
+			MA="${adjacencies}"
+			
+	else
+		MAparam="-m"
+		MA="${markers}"
+		
+fi
+
+if [ -z $processNumber ]
+	then
+		echo "Using only one process"
+		processNumber="1"
+fi
+
+if [ -z $x ]
+	then
+		echo "default: x=0 "
+		x="0"
+fi
+
+if [ -z $kT ]
+	then
+		echo "default: kT=0.1 "
+		kT="0.1"
+fi
+
+if [ -z $alpha ]
+	then
+		echo "default: alpha=0.0"
+		alpha="0.0"
+fi
+
+if [ -z $sampleNumber ]
+	then
+		echo "No Samples"
+		sampleNumber="0"
+fi
+
+if [ -z $out ]
+    then
+        echo "No directory for preprocessing output given."
+        exit
+    elif [ -d $out ]
+	     then
+		    echo "directory ${out} is already there."
+	else
+	    echo "create directory ${out}"
+	    mkdir ${out}
+				
+fi
+
+#preprocessing: weighting With DeClone
+python ./weightingWithDeClone.py ${MAparam} ${MA} ${treeparam} ${tree} -kT ${kT} -out ${out}
+
+extant="${out}/extant_adjacencies"
+internal="${out}/weighted_internal_adjacencies"
+nhx_tree="${out}/nhx_tree"
+#create directories for each sample run
+for ((i=0;i<$processNumber;i+=1))
+	do 
+		if [ -d ${PWD}/test_${i} ]
+			then
+				echo "directory ${PWD}/test_${i} is already there."
+			else
+				echo "create directory ${PWD}/test_${i}"
+				mkdir ${PWD}/test_${i}
+				
+		fi
+		directories[i]="${PWD}/test_${i}"
+	done
+
+
+START=$(date +%s.%N)
+
+for ((i=0;i<$processNumber;i+=1))
+	do
+		cd ${directories[i]}
+		if [ $i -eq 0 ]
+			then
+			#PhySca call with 0th sample
+				python ../Main.py -tree $nhx_tree -alpha $alpha -s $sampleNumber -x $x -extant $extant -internal $internal -out ${PWD} -sc $i > ${PWD}/log.txt&
+		else
+		    #PhySca call without 0th sample
+			python ../Main.py -tree $nhx_tree -alpha $alpha -s $sampleNumber -x $x -extant $extant -internal $internal -out ${PWD} -sk -sc $(($i+$sampleNumber-1)) > ${PWD}/log.txt&
+		
+		fi
+		cd ..
+	done
+	wait
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo ${DIFF}
+
+#Now the different outputs need to be put together
+#Common output folder
+if [ -d ${PWD}/output ]
+			then
+				echo "directory ${PWD}/output is already there."
+			else
+				echo "create directory ${PWD}/output"
+				mkdir ${PWD}/output
+fi
+outputDir=${PWD}/output
+
+printf "Combined log file of ${i} runs, each with ${sampleNumber} samples" > $outputDir/log.txt
+printf "Combined SCJ distances of ${i} runs, each with ${sampleNumber} samples\n" > $outputDir/SCJ_distances
+
+#First of all: files doubled_scaffolds_*, undoubled_scaffolds_*, reconstructed_adjacencies_*
+#then the files, which needs to be combined: SCJ_distances, log.txt, statistic_allSampled_ReconstructedAdjacencies
+#and conflict (only for the first process)
+for ((i=0;i<$processNumber;i+=1))
+	do
+	    if [ $i -eq 0 ]
+			then
+		        mv ${directories[i]}/conflicts $outputDir
+	    fi
+        mv ${directories[i]}/doubled_scaffolds* $outputDir
+        mv ${directories[i]}/undoubled_scaffolds* $outputDir
+        mv ${directories[i]}/reconstructed_adjacencies* $outputDir
+        cat ${directories[i]}/log.txt >> $outputDir/log.txt
+        rm ${directories[i]}/log.txt
+        cat ${directories[i]}/SCJ_distances >> $outputDir/temp_distances
+        rm ${directories[i]}/SCJ_distances
+        fileList[i]=${directories[i]}/statistic_allSampled_ReconstructedAdjacencies
+
+    done
+flist=$(printf ",%s" "${fileList[@]}")
+python putStatisticFilesTogether.py ${flist:1} ${outputDir}/statistic_allSampled_ReconstructedAdjacencies
+
+#"  " in the sort statement is an tab
+cat ${outputDir}/temp_distances | sort -t " " -nk1 >> ${outputDir}/SCJ_distances
+rm ${outputDir}/temp_distances
+for ((i=0;i<$processNumber;i+=1))
+    do
+       rm  ${directories[i]}/statistic_allSampled_ReconstructedAdjacencies
+       rm -r ${directories[i]}
+    done
+
+echo "Done"
